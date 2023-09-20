01141c1adf7e577d43782ba4bdd782dd49fb3658
Washington Rodrigues <washington.rodrigues@redgtech.com.br>
2023-09-20 14:55:13 -0300

    home atualizada para novo estilo

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,8 +1,13 @@
 import style from './style.css';
-import { motion } from 'framer-motion';
+import { motion, useScroll } from 'framer-motion';
 import NextPage from '../NextPage';
+import { useState } from 'react';
 
 export default function Home() {
+  const [hovered, setHovered] = useState(false);
+  const styleBtn = {
+    transition: hovered ? '.3s' : '',
+  };
   function downloadFile() {
     const googleDriveFileLink =
       'https://drive.google.com/u/1/uc?id=1UPPez9b10BA-7bGNFjK_bHUpvx4Wrk3c&export=download';
@@ -16,43 +21,128 @@ export default function Home() {
     >
       <div className="first-content ">
         <div className="title">
-          <h2 className="welcome-title">Seja bem-vindo(a)!</h2>
           <h1 className="name-title">Washington Rodrigues</h1>
           <h2 className="title-title">
             Desenvolvedor Front-end e UI/UX Designer
           </h2>
+          <p className="p-bio">
+            Utilizando o ReactJs como principal tecnologia, me dedico à criação
+            de aplicações web sofisticadas e altamente responsivas com foco na
+            experiência do usuário. Busco oferecer soluções que não apenas
+            atendam às necessidades, mas também encantem os usuários.
+          </p>
         </div>
-        <ul className="contact-links">
-          <li>
-            <a href="mailto:washington_sr_@hotmail.com">
-              <i class="fa-regular fa-envelope"></i>washington_sr_@hotmail.com
-            </a>
-          </li>
-          <li>
-            <a target="_blank" href="https://wa.me/5579999216703">
-              <i class="fa-brands fa-whatsapp"></i>+55 79 9 9921-6703
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              href="https://www.google.com/maps/place/Aracaju,+SE/@-11.0059634,-37.2679689,11z/data=!3m1!4b1!4m6!3m5!1s0x71ab04015be27cd:0x804434fd92ec3b36!8m2!3d-10.9266526!4d-37.0729014!16zL20vMDFoeTg0?entry=ttu"
-            >
-              <i class="fa-solid fa-location-dot"></i>Aracaju, Sergipe - Brasil
-            </a>
-          </li>
-        </ul>
-        <button onClick={downloadFile} className="download-cv">
+        <div className="technologies">
+          <span
+            data-aos="fade-up"
+            data-aos-delay="200"
+            data-aos-duration="1000"
+          >
+            <img
+              className="tech-icons"
+              src="../.././public/icons/react.svg"
+              alt=""
+            />
+            <p className="tech-name">ReactJS</p>
+          </span>
+          <span
+            data-aos="fade-up"
+            data-aos-delay="220"
+            data-aos-duration="1100"
+          >
+            <img
+              className="tech-icons"
+              src="../.././public/icons/javascript.svg"
+              alt=""
+            />
+            <p className="tech-name">JavaScript</p>
+          </span>
+          <span
+            data-aos="fade-up"
+            data-aos-delay="240"
+            data-aos-duration="1200"
+          >
+            <img
+              className="tech-icons"
+              src="../.././public/icons/typescript.svg"
+              alt=""
+            />
+            <p className="tech-name">TypeScript</p>
+          </span>
+          <span
+            data-aos="fade-up"
+            data-aos-delay="260"
+            data-aos-duration="1300"
+          >
+            <img
+              className="tech-icons"
+              src="../.././public/icons/html.svg"
+              alt=""
+            />
+            <p className="tech-name">HTML5</p>
+          </span>
+          <span
+            data-aos="fade-up"
+            data-aos-delay="280"
+            data-aos-duration="1400"
+          >
+            <img
+              className="tech-icons"
+              src="../.././public/icons/css.svg"
+              alt=""
+            />
+            <p className="tech-name">CSS3</p>
+          </span>
+          <span
+            data-aos="fade-up"
+            data-aos-delay="300"
+            data-aos-duration="1500"
+          >
+            <img
+              className="tech-icons"
+              src="../.././public/icons/git.svg"
+              alt=""
+            />
+            <p className="tech-name">Git</p>
+          </span>
+          <span
+            data-aos="fade-up"
+            data-aos-delay="320"
+            data-aos-duration="1600"
+          >
+            <img
+              className="tech-icons"
+              src="../.././public/icons/figma.svg"
+              alt=""
+            />
+            <p className="tech-name">Figma</p>
+          </span>
+        </div>
+        <button
+          data-aos="fade-right"
+          data-aos-delay="200"
+          data-aos-duration="2000"
+          onClick={downloadFile}
+          className="download-cv"
+          onMouseEnter={() => setHovered(true)}
+          style={styleBtn}
+        >
           <i class="fa-solid fa-download"></i>Download CV
         </button>
         <NextPage to={'/projects'} />
       </div>
-      <div className="second-content">
-        <img
-          className="photo"
-          src="../../public/profile.jpg"
-          alt="Foto Washington Rodrigues"
-        />
+      <div
+        className="second-content"
+        data-aos="fade-down"
+        data-aos-duration="2000"
+      >
+        <a href="/about">
+          <img
+            className="photo"
+            src="../../public/profile2.jpg"
+            alt="Foto Washington Rodrigues"
+          />
+        </a>
       </div>
     </motion.header>
   );
